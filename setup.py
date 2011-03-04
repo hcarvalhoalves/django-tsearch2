296ced177da84650ee89a5c084e5091b431e80f4
Henrique Carvalho Alves <hcarvalhoalves@gmail.com.br>
2011-03-04 17:11:11 -0300

    Install script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+try:
+    import ez_setup
+    ez_setup.use_setuptools()
+except ImportError:
+    pass
+
+
+setup(
+    name = "django-tsearch2",
+    version = "0.1",
+    packages = ['tsearch2', 'tsearch2.management', 'tsearch2.management.commands'],
+    author = "Henrique Carvalho Alves",
+    author_email = "hcarvalhoalves@gmail.com",
+    description = "TSearch2 support for Django",
+    url = "http://github.com/hcarvalhoalves/django-tsearch2",
+)
