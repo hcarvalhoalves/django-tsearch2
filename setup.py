6e0f2880c80150a71cc719ff652f1bfbde08a1fa
Henrique Carvalho Alves <henrique@redecolibri.com.br>
2011-03-09 22:15:24 -0300

    Mark as zip_safe = False

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup
-try:
-    import ez_setup
-    ez_setup.use_setuptools()
-except ImportError:
-    pass
-
 
 setup(
     name = "django-tsearch2",
     version = "0.2",
     packages = ['tsearch2', 'tsearch2.management', 'tsearch2.management.commands'],
+    zip_safe = False,
     author = "Henrique Carvalho Alves",
     author_email = "hcarvalhoalves@gmail.com",
     description = "TSearch2 support for Django",
