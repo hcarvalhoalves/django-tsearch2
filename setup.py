59a3f776edb913d0064e1f6439ca81e6bd573861
Henrique Carvalho Alves <hcarvalhoalves@gmail.com>
2012-11-28 01:24:43 -0200

    - Include LICENSE file - Up-to-date setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,25 @@ from setuptools import setup
 setup(
     name = "django-tsearch2",
     version = "0.3.0",
-    packages = ['tsearch2', 'tsearch2.management', 'tsearch2.management.commands'],
-    zip_safe = False,
+    url = "http://github.com/hcarvalhoalves/django-tsearch2",
+    description = "Postgresql's full text search support for Django",
     author = "Henrique Carvalho Alves",
     author_email = "hcarvalhoalves@gmail.com",
-    description = "Postgresql's full text search support for Django",
-    url = "http://github.com/hcarvalhoalves/django-tsearch2",
+    packages = [
+        'tsearch2',
+        'tsearch2.management',
+        'tsearch2.management.commands'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe = False,
 )
